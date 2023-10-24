0c0c8e403081d0689a7b18806881d214a64d2dfd
VinhHuuTran <tranhuuvinh1109@gmail.com>
2023-10-24 15:38:27 +0700

    done

--- manage_user/urls.py
+++ manage_user/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('create_new_project/', views.create_new_project, name='create_new_project'),
 		path('get_all_projects/', views.get_all_projects, name='get_all_projects'),
+		path('camera/<str:name>/', views.video_stream, name='video_stream'),
 ]
